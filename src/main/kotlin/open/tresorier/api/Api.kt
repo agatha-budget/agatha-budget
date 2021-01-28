25ac6968424d05ded7268211837d9d3d00fa9d36
Erica <erica@tresorier.me>
2021-01-28 12:58:47 +0100

    fix

--- src/main/kotlin/open/tresorier/api/Api.kt
+++ src/main/kotlin/open/tresorier/api/Api.kt
@@ -13,35 +13,34 @@ fun main() {
 
     val properties = Properties.getProperties()
     // Session Manager
-    SuperTokens.config().withHosts(properties.getProperty("supertoken_url"), properties.getProperty("supertoken_api_key"))
+    SuperTokens.config()
+        .withHosts(properties.getProperty("supertoken_url"), properties.getProperty("supertoken_api_key"))
 
     ServiceManager.start()
 
     val app = Javalin.create { config ->
                                    config.enableCorsForOrigin(properties.getProperty("allowed_origin"))
-    }.apply {
-        exception(Exception::class.java) { e, _ -> e.printStackTrace() }
     }.start(getHerokuAssignedOrDefaultPort())
 
     app.exception(SuperTokensException::class.java) { _, _ ->
         SuperTokens.exceptionHandler()
-            .onTryRefreshTokenError {_, ctx ->
-                    ctx.status(401).result("Call the refresh API");
-                }
-            .onUnauthorisedError { _ , ctx ->
-                                      ctx.status(401).result("Please login again")
+            .onTryRefreshTokenError { _, ctx ->
+                ctx.status(401).result("Call the refresh API");
+            }
+            .onUnauthorisedError { _, ctx ->
+                ctx.status(401).result("Please login again")
             }
             .onGeneralError { exception, ctx ->
                                  ctx.status(401).result(exception.message ?: "no message")
             }
             .onTokenTheftDetectedError { exception, ctx ->
                                             ctx.status(401).result("You are being attacked");
-                                            // revoke affected session.
-                                            try {
-                                                SuperTokens.revokeSession(exception.sessionHandle);
-                                            } catch (e : GeneralException) {
-                                                e.printStackTrace()
-                                            }
+                // revoke affected session.
+                try {
+                    SuperTokens.revokeSession(exception.sessionHandle);
+                } catch (e: GeneralException) {
+                    e.printStackTrace()
+                }
             }
     }
 
@@ -60,13 +59,13 @@ fun main() {
     }
 
     app.post("/person") { ctx ->
-                              val name = ctx.queryParam<String>("name").get()
-                          val password = ctx.queryParam<String>("password").get()
-                          val email = ctx.queryParam<String>("email").get()
-                          val person : Person? = ServiceManager.personService.createPerson(name, password, email)
-                          if (person == null) {
-                              ctx.status(400)
-                              ctx.json("the user wasn't created")
+        val name = ctx.queryParam<String>("name").get()
+        val password = ctx.queryParam<String>("password").get()
+        val email = ctx.queryParam<String>("email").get()
+        val person: Person? = ServiceManager.personService.createPerson(name, password, email)
+        if (person == null) {
+            ctx.status(400)
+            ctx.json("the user wasn't created")
                           } else {
                               ctx.status(200)
                               ctx.json(person.id)
@@ -77,12 +76,12 @@ fun main() {
     app.post("/session/refresh") { ctx -> ctx.result("refreshed") }
 
     app.post("/login") { ctx ->
-                             val email = ctx.queryParam<String>("email").get()
-                         val password = ctx.queryParam<String>("password").get()
-                         val person : Person? = ServiceManager.personService.login(email, password)
-                         if (person == null) {
-                             val unlockingDate = ServiceManager.personService.getUnlockingDateForEmail(email)
-                             ctx.status(400)
+        val email = ctx.queryParam<String>("email").get()
+        val password = ctx.queryParam<String>("password").get()
+        val person: Person? = ServiceManager.personService.login(email, password)
+        if (person == null) {
+            val unlockingDate = ServiceManager.personService.getUnlockingDateForEmail(email)
+            ctx.status(400)
                              ctx.json("{'unlockingDate' : " + unlockingDate + "}")
                          } else {
                              ctx.status(200)
@@ -105,14 +104,13 @@ fun main() {
                             ctx.header("logged", "trueee")
     }
     app.post("/budget") { ctx ->
-                              val validSession = SuperTokens.getFromContext(ctx)
-                          if (validSession == null) {
-                              ctx.result("invalid session")
-                          }
-                          else {
-                              ctx.result(validSession.userId)
-                          }
-                          /*
+        val validSession = SuperTokens.getFromContext(ctx)
+        if (validSession == null) {
+            ctx.result("invalid session")
+        } else {
+            ctx.result(validSession.userId)
+        }
+        /*
                            val userId = validSession.userId
                            val user : Person? = ServiceManager.personService.getById(userId)
                            user?.let {
@@ -136,7 +134,7 @@ fun main() {
 
 }
 
-private fun getHerokuAssignedOrDefaultPort() : Int {
+private fun getHerokuAssignedOrDefaultPort(): Int {
     val herokuPort = System.getenv("PORT");
     if (herokuPort != null) {
         return Integer.parseInt(herokuPort);
