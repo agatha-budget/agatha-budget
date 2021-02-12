58cbcb71f52441c99821bf63af4d1d112df8a415
Erica <erica@tresorier.me>
2021-02-12 19:34:18 +0100

    add integration properties

--- src/main/kotlin/open/tresorier/utils/Properties.kt
+++ src/main/kotlin/open/tresorier/utils/Properties.kt
@@ -14,7 +14,7 @@ object Properties {
         val properties = Properties(default)
 
         val tresorierDB = "HEROKU_POSTGRESQL_AQUA_JDBC"
-        val testDB = "HEROKU_POSTGRESQL_GREEN_JDBC"
+        val integrationDB = "HEROKU_POSTGRESQL_GREEN_JDBC"
 
         val tresorier_db_url = System.getenv(tresorierDB +"_URL") ?: default.getProperty("tresorier_db_url_dflt")
         val tresorier_db_usr = System.getenv(tresorierDB + "_USERNAME") ?: default.getProperty("tresorier_db_usr_dflt")
@@ -24,6 +24,10 @@ object Properties {
         val test_db_usr = default.getProperty("test_db_usr_dflt")
         val test_db_pwd = default.getProperty("test_db_pwd_dflt")
 
+        val integration_db_url = System.getenv(integrationDB +"_URL") ?: default.getProperty("integration_db_url_dflt")
+        val integration_db_usr = System.getenv(integrationDB + "_USERNAME") ?: default.getProperty("integration_db_usr_dflt")
+        val integration_db_pwd = System.getenv(integrationDB + "_PASSWORD") ?: default.getProperty("integration_db_pwd_dflt")
+
         properties.setProperty("tresorier_db_url", tresorier_db_url)
         properties.setProperty("tresorier_db_usr", tresorier_db_usr)
         properties.setProperty("tresorier_db_pwd", tresorier_db_pwd)
@@ -31,6 +35,10 @@ object Properties {
         properties.setProperty("test_db_url", test_db_url)
         properties.setProperty("test_db_usr", test_db_usr)
         properties.setProperty("test_db_pwd", test_db_pwd)
+
+        properties.setProperty("integration_db_url", integration_db_url)
+        properties.setProperty("integration_db_usr", integration_db_usr)
+        properties.setProperty("integration_db_pwd", integration_db_pwd)
 
         return properties
     }
