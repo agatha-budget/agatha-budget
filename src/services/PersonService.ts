b705613f6dfca50b073d13766c41fd1bca28e4c2
Erica <erica.web@odena.eu>
2021-03-10 17:53:28 +0100

    update login response

--- src/services/PersonService.ts
+++ src/services/PersonService.ts
@@ -2,12 +2,29 @@ import { State } from '@/store/index'
 import { Store } from 'vuex'
 import { personApi } from '@/services/api/apis'
 import router from '@/router'
+import axios from 'axios'
+
+type loginResponse = {
+    name: string;
+    unlockingDate: number;
+}
 
 class PersonService {
-  public async createSession (store: Store<State>, email: string, password: string, options?: any) {
-    await personApi.createSession(email, password, options)
-    store.dispatch('updateLogged')
-    router.push('/')
+  public async createSession (store: Store<State>, email: string, password: string, options?: any): Promise<loginResponse> {
+    let data
+    let response
+    try {
+      response = await personApi.createSession(email, password, options)
+      data = response.data
+      store.dispatch('updateLogged')
+      router.push('/')
+    } catch (exception) {
+      if (axios.isAxiosError(exception)) {
+        response = exception.response
+        data = (response) ? response.data : {}
+      }
+    }
+    return JSON.parse(data)
   }
 
   public async deleteSession (store: Store<State>, options?: any) {
