0ed03d2df4aa030b98a59d5380b623fad0a09bdb
Erica <erica.web@odena.eu>
2021-03-25 12:31:12 +0100

    add accountApi to availables apis

--- src/services/api/apis.ts
+++ src/services/api/apis.ts
@@ -1,3 +1,4 @@
-import { PersonApi } from './openApi/api'
+import { PersonApi, AccountApi } from './openApi/api'
 
 export const personApi = new PersonApi()
+export const accountApi = new AccountApi()
