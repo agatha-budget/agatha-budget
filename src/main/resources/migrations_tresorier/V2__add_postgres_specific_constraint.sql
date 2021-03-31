756c7f694f488f02e85e6792103091b5a646676e
Erica <erica@tresorier.me>
2021-03-31 12:19:41 +0200

    update database model

--- src/main/resources/migrations_tresorier/V2__add_postgres_specific_constraint.sql
+++ src/main/resources/migrations_tresorier/V2__add_postgres_specific_constraint.sql
@@ -1,20 +1,24 @@
-create or replace function are_category_and_account_from_same_budget (_category_id VARCHAR(36), _accound_id VARCHAR(36))
+create or replace function are_category_and_account_from_same_budget (_category_id VARCHAR(36), _account_id VARCHAR(36))
   returns boolean as $$
 declare
   	same_budget boolean;
   	universal_category boolean;
 begin
+    -- get the master category of the given category and assert its budget is the same than the given account's
    	select count(*) = 1 into same_budget
         from account acc
-             join category cat
-               on cat.budget_id = acc.budget_id
+        join master_category master_cat
+            on master_cat.budget_id = acc.budget_id
+        join category cat
+            on cat.master_category_id = master_cat.id
         where cat.id = _category_id
-            and acc.id = _accound_id;
+            and acc.id = _account_id;
 
+    -- if the master_category of the given category is null it means it is an universal category (like income or transfert)
 	select count(*) = 1 into universal_category
         from category cat
         where cat.id = _category_id
-            and cat.budget_id is null;
+            and cat.master_category_id is null;
 
     return same_budget or universal_category;
 end;
--- src/main/resources/migrations_tresorier/all_db/V3__Add_generic_categories.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO
-       category (id, name)
-VALUES
-        ('universal_income_category', 'I18N_INCOME'),
-        ('universal_transfert_category', 'I18N_TRANSFERT')
-;
