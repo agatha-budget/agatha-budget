f3f2a2353bfffb286b5e74a18c5924fed9a4d907
Erica <erica@tresorier.me>
2021-04-01 15:55:33 +0200

    add get max timestamp for month in GMT

--- src/test/kotlin/open/tresorier/utils/TimeTest.kt
+++ src/test/kotlin/open/tresorier/utils/TimeTest.kt
@@ -1,5 +1,6 @@
 package open.tresorier.utils
 
+import open.tresorier.model.Month
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
@@ -8,5 +9,11 @@ class TimeTest {
         val myBirthdate : Long = 715231200000
         val whenIm3HoursOld : Long = 715242912000
         assertEquals(whenIm3HoursOld, myBirthdate + Time.getDuration(hours=3, minutes=15, seconds=12))
+    }
+
+    @Test fun getTimestampForGivenMonth(){
+        val lastMilliSecondOfMarch2141 = 5404060800-1
+        val march2141 = Month(3, 2141)
+        assertEquals(lastMilliSecondOfMarch2141, Time.getMaxTimestamp(march2141))
     }
 }
