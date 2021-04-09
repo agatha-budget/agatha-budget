581e0af6d2987ee333a757b59b522f5795280a18
Erica <erica@tresorier.me>
2021-04-09 19:46:05 +0200

    change getById by getByObject

--- src/main/kotlin/open/tresorier/dao/IAccountDao.kt
+++ src/main/kotlin/open/tresorier/dao/IAccountDao.kt
@@ -1,12 +1,13 @@
 package open.tresorier.dao
 
 import open.tresorier.model.Account
+import open.tresorier.model.Budget
 import open.tresorier.model.Person
 
 interface IAccountDao {
     fun insert(account: Account) : Account
     fun update(account: Account) : Account
     fun getById(id: String): Account
-    fun findByBudgetId(budgetId: String) : List<Account>
+    fun findByBudget(budget: Budget) : List<Account>
     fun getOwner(account: Account) : Person
 }
