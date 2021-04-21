695d64e275103083730233cc90e38f1689dba0c7
Erica <erica.web@odena.eu>
2021-04-21 15:28:15 +0200

    add table of operation v1

--- src/utils/Time.ts
+++ src/utils/Time.ts
@@ -13,4 +13,21 @@ export default class Time {
     const day = dateTime.getDate()
     return year * 10000 + month * 100 + day
   }
+
+  public static getMonthAsDate (monthAsInt: number): Date {
+    const date = new Date()
+    const month = monthAsInt % 100
+    const year = (monthAsInt - month) / 100
+    date.setMonth(month)
+    date.setFullYear(year)
+    return date
+  }
+
+  public static getDayAsDate (dayAsInt: number): Date {
+    const day = dayAsInt % 100
+    const monthAsInt = (dayAsInt - day) / 100
+    const date = this.getMonthAsDate(monthAsInt)
+    date.setDate(day)
+    return date
+  }
 }
--- src/views/AccountPage.vue
+++ src/views/AccountPage.vue
@@ -1,7 +1,23 @@
 <template >
   <div :class="this.$store.state.css">
     <div class="home row">
-      <h1>test - </h1>
+      <h1>test - {{ this.account.name }}</h1>
+      <table class="operationTable table" >
+          <tr class="">
+            <th class="date"><div>{{ $t("DATE") }}</div></th>
+            <th class="category">{{ $t("CATEGORY") }}</th>
+            <th class="memo">{{ $t("MEMO") }}</th>
+            <th class="amount">{{ $t("AMOUNT") }}</th>
+          </tr>
+          <tbody>
+          <tr class="operation" v-for="operation in this.operations" :key="operation">
+            <td class="date"><div>{{ $d(this.getDayAsDate(operation.day.comparable), 'day') }}</div></td>
+            <td class="category">{{ operation.categoryId }}</td>
+            <td class="memo">{{ operation.memo }}</td>
+            <td class="amount">{{ operation.amount }}</td>
+          </tr>
+          </tbody>
+      </table>
     </div>
   </div>
 </template>
@@ -10,6 +26,8 @@
 import { defineComponent } from 'vue'
 import { redirectToLoginPageIfNotLogged } from '@/router'
 import { Account } from '@/model/model'
+import Time from '@/utils/Time'
+import { operationService } from '@/services/OperationService'
 
 interface AccountPageData {
     account: Account;
@@ -23,13 +41,14 @@ export default defineComponent({
   },
   created: async function () {
     redirectToLoginPageIfNotLogged(this.$store)
+    this.getAccountOperation()
   },
   props: ['accountId'],
   data (): AccountPageData {
     return {
       account: {
         id: this.accountId,
-        name: '',
+        name: 'the account name',
         amount: 0
       },
       operations: {}
@@ -37,7 +56,14 @@ export default defineComponent({
   },
   methods: {
     async getAccountOperation () {
-      // to do
+      return operationService.getOperations(this.account).then(
+        (operations) => {
+          this.operations = operations
+        }
+      )
+    },
+    getDayAsDate (dayAsInt: number): Date {
+      return Time.getDayAsDate(dayAsInt)
     }
   }
 })
