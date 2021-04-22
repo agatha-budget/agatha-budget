0bc2149a4e3afd40e0d17a2e508e5996ca7e9293
Erica <erica@tresorier.me>
2021-04-22 19:44:09 +0200

    init budget with basic categories

--- src/main/kotlin/open/tresorier/dependenciesinjection/service_module.kt
+++ src/main/kotlin/open/tresorier/dependenciesinjection/service_module.kt
@@ -1,14 +1,12 @@
 package open.tresorier.dependenciesinjection
 
-import open.tresorier.dao.*
-import open.tresorier.dao.jooq.pgsql.*
 import open.tresorier.services.*
 import org.koin.dsl.module
 
 val service_module = module {
     single { AuthorizationService(get(), get(), get(), get(), get(), get()) }
     single { PersonService(get(), get()) }
-    single { BudgetService(get(), get()) }
+    single { BudgetService(get(), get(), get(), get()) }
     single { AccountService(get(), get(), get()) }
     single { CategoryService(get(), get()) }
     single { MasterCategoryService(get(), get()) }
