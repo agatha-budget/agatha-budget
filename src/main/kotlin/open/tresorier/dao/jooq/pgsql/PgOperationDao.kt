25fbd7678e931265c0251f243d5f30a51d82790a
Erica <erica@tresorier.me>
2021-04-22 13:17:59 +0200

    rename var

--- src/main/kotlin/open/tresorier/dao/jooq/pgsql/PgOperationDao.kt
+++ src/main/kotlin/open/tresorier/dao/jooq/pgsql/PgOperationDao.kt
@@ -72,8 +72,8 @@ class PgOperationDao(val configuration: Configuration) : IOperationDao {
         val jooqSpendingList = query.fetch()
         val spendingList: MutableList<Spending> = mutableListOf()
         for (spendingRecord in jooqSpendingList) {
-            val allocation = this.toSpending(spendingRecord)
-            spendingList.add(allocation)
+            val spending = this.toSpending(spendingRecord)
+            spendingList.add(spending)
         }
         return spendingList
     }
