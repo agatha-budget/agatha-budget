05cd8812c4184c7f23a34a919eef3c1f06a33cc7
Claire <claire@agatha.fr>
2021-10-29 15:36:24 +0200

    correction responsive

--- src/views/AccountPage.vue
+++ src/views/AccountPage.vue
@@ -1,40 +1,68 @@
 <template >
   <div :class="this.$store.state.css">
     <div class="accountPage row col-lg-10 offset-lg-1 col-xl-8 offset-xl-2">
-        <div v-if="this.editingTitle" class="row">
-          <span class="name col-4 offset-4">
-            <input id="accountName" class="form-control" v-model="nameComputed">
-          </span>
-        <span class="validation col">
-           <button class="btn fas fa-check" v-on:click="newName()"/>
-           <button class="btn fas fa-times" v-on:click="this.cancelEditing()"/>
+      <div v-if="this.editingTitle" class="row">
+        <span class="name col-4 offset-4">
+          <input id="accountName" class="form-control" v-model="nameComputed" />
         </span>
-        </div>
-          <div v-else class="editableNameAccount">
+        <span class="validation col">
+          <button class="btn fas fa-check" v-on:click="newName()" />
+          <button class="btn fas fa-times" v-on:click="this.cancelEditing()" />
+        </span>
+      </div>
+      <div v-else class="editableNameAccount">
         <a v-on:click="this.displayTitleEditing()">
-          <h1> {{ (this.account) ? this.account.name : ''}} : {{ (this.account) ? getEurosAmount(this.account.amount) : ''}}€
-            <button class="btn fas fa-pen"/>
+          <h1>
+            {{ this.account ? this.account.name : "" }} :
+            {{ this.account ? getEurosAmount(this.account.amount) : "" }}€
+            <button class="btn fas fa-pen" />
           </h1>
         </a>
       </div>
-      <div class="operationTable table-hover" >
-          <tbody>
-          <OperationForm class="operationCreate" @update-operation-list="getAccountOperation" :accountId="this.accountId" />
+      <div class="operationTable table-hover">
+        <tbody>
+          <OperationForm
+            class="operationCreate"
+            @update-operation-list="getAccountOperation"
+            :accountId="this.accountId"
+          />
           <template v-for="operation in this.operations" :key="operation">
-            <OperationForm v-if="operation.editing" @update-operation-list="getAccountOperation" :accountId="this.accountId" :operation="operation"/>
+            <OperationForm
+              v-if="operation.editing"
+              @update-operation-list="getAccountOperation"
+              :accountId="this.accountId"
+              :operation="operation"
+            />
             <div class="operation storedOperation v-else">
-              <div class="date col-2 offset-1"><div>{{ $d(this.getDayAsDate(operation.day), 'day') }}</div></div>
+              <div class="date col-2 offset-1">
+                <div>{{ $d(this.getDayAsDate(operation.day), "day") }}</div>
+              </div>
               <div class="col-8"></div>
-              <div class="category col-3 offset-1">{{ this.getCategoryById(operation.categoryId)?.name ?? $t("UNKNOWN_CATEGORY") }}</div>
-              <div class="amount col-2 offset-2">{{ this.getEurosAmount(operation.amount) }} €</div>
+              <div class="category col-3 offset-1">
+                {{
+                  this.getCategoryById(operation.categoryId)?.name ??
+                  $t("UNKNOWN_CATEGORY")
+                }}
+              </div>
+              <div class="amount col-2 offset-2">
+                {{ this.getEurosAmount(operation.amount) }} €
+              </div>
               <div class="action col-1 offset-1">
-                <button class="btn fas fa-pen" v-on:click="setAsEditing(operation)" :title="$t('EDIT')"/>
-                <button class="btn fas fa-trash" v-on:click="deleteOperation(operation)" :title="$t('DELETE')"/>
+                <button
+                  class="btn fas fa-pen"
+                  v-on:click="setAsEditing(operation)"
+                  :title="$t('EDIT')"
+                />
+                <button
+                  class="btn fas fa-trash"
+                  v-on:click="deleteOperation(operation)"
+                  :title="$t('DELETE')"
+                />
               </div>
               <div class="memo col-3 offset-1">{{ operation.memo }}</div>
-           </div>
+            </div>
           </template>
-          </tbody>
+        </tbody>
       </div>
     </div>
   </div>
@@ -51,11 +79,11 @@ import OperationForm from '@/components/forms/OperationForm.vue'
 import Utils from '@/utils/Utils'
 
 interface AccountPageData {
-    operations: EditableOperation[];
+  operations: EditableOperation[];
 }
 
 interface EditableOperation extends Operation {
-    editing: boolean;
+  editing: boolean;
 }
 
 export default defineComponent({
@@ -110,11 +138,9 @@ export default defineComponent({
       return Time.getDateFromDay(dayAsInt)
     },
     deleteOperation (operation: Operation) {
-      OperationService.deleteOperation(this.$store, operation).then(
-        () => {
-          this.getAccountOperation()
-        }
-      )
+      OperationService.deleteOperation(this.$store, operation).then(() => {
+        this.getAccountOperation()
+      })
     },
     setAsEditing (operation: EditableOperation) {
       operation.editing = true
@@ -122,17 +148,15 @@ export default defineComponent({
     operationToEditableOperation (operations: Operation[]): EditableOperation[] {
       const editableOperations: EditableOperation[] = []
       operations.forEach((operation) =>
-        editableOperations.push(
-          {
-            id: operation.id,
-            day: operation.day,
-            accountId: operation.accountId,
-            categoryId: operation.categoryId,
-            amount: operation.amount,
-            memo: operation.memo,
-            editing: false
-          }
-        )
+        editableOperations.push({
+          id: operation.id,
+          day: operation.day,
+          accountId: operation.accountId,
+          categoryId: operation.categoryId,
+          amount: operation.amount,
+          memo: operation.memo,
+          editing: false
+        })
       )
       return editableOperations
     },
