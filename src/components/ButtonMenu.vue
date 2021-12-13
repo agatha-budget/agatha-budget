0eea842154c9e6b1ce1093273f98d24ffb808543
Claire <claire@agatha.fr>
2021-12-13 12:08:14 +0100

    creation of a new component & scrollable operations

---
+++ src/components/ButtonMenu.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="buttonMenu">
+        <ul id="actionsList" class="list-group list-group-horizontal d-flex justify-content-center">
+        <li><button v-on:click="goHomePage" class="btn fas fa-home" :title="$t('HOME')"/></li>
+        <li><button class="btn fas fa-chart-line disabled" :title="$t('GRAPH_AND_REPORT')"/></li>
+        <li><button class="btn fas fa-cog disabled" :title="$t('PREFERENCES')"/></li>
+        <li><button v-on:click="logout" class="btn fas fa-sign-out-alt" :title="$t('LOGOUT')"/></li>
+      </ul>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'BtnMenu'
+})
+</script>
--- src/views/AccountPage.vue
+++ src/views/AccountPage.vue
@@ -21,33 +21,30 @@
       </div>
       <div class="operationTable table-hover">
         <OperationForm class="operationCreate" @update-operation-list="getAccountOperation" :accountId="this.accountId"/>
-        <template v-for="operation in this.operations" :key="operation">
-          <OperationForm v-if="operation.editing" @update-operation-list="getAccountOperation" :accountId="this.accountId" :operation="operation"/>
-          <div v-else class="operation storedOperation">
-            <div class="date col-2 offset-1">
-              <div>{{ $d(this.getDayAsDate(operation.day), "day") }}</div>
+        <div class="scrollable">
+          <template v-for="operation in this.operations" :key="operation">
+            <OperationForm v-if="operation.editing" @update-operation-list="getAccountOperation" :accountId="this.accountId" :operation="operation"/>
+            <div v-else class="operation storedOperation">
+              <div class="date col-2 offset-1">
+                <div>{{ $d(this.getDayAsDate(operation.day), "day") }}</div>
+              </div>
+              <div class="col-8"></div>
+              <div class="category col-3 offset-1">
+                {{ this.getCategoryById(operation.categoryId)?.name ?? $t("UNKNOWN_CATEGORY") }}
+              </div>
+              <div class="amount col-2 offset-2">
+                {{ this.getEurosAmount(operation.amount) }} €
+              </div>
+              <div class="action col-1 offset-1">
+                <button class="btn fas fa-pen" v-on:click="setAsEditing(operation)" :title="$t('EDIT')"/>
+                <button class="btn fas fa-trash" v-on:click="deleteOperation(operation)" :title="$t('DELETE')"/>
+              </div>
+              <div class="memo col-3 offset-1">{{ operation.memo }}</div>
             </div>
-            <div class="col-8"></div>
-            <div class="category col-3 offset-1">
-              {{ this.getCategoryById(operation.categoryId)?.name ?? $t("UNKNOWN_CATEGORY") }}
-            </div>
-            <div class="amount col-2 offset-2">
-              {{ this.getEurosAmount(operation.amount) }} €
-            </div>
-            <div class="action col-1 offset-1">
-              <button class="btn fas fa-pen" v-on:click="setAsEditing(operation)" :title="$t('EDIT')"/>
-              <button class="btn fas fa-trash" v-on:click="deleteOperation(operation)" :title="$t('DELETE')"/>
-            </div>
-            <div class="memo col-3 offset-1">{{ operation.memo }}</div>
-          </div>
-        </template>
-        <ul id="actionsList" class="list-group list-group-horizontal d-flex justify-content-center">
-            <li><button v-on:click="goHomePage" class="btn fas fa-home" :title="$t('HOME')"/></li>
-            <li><button class="btn fas fa-chart-line disabled" :title="$t('GRAPH_AND_REPORT')"/></li>
-            <li><button class="btn fas fa-cog disabled" :title="$t('PREFERENCES')"/></li>
-            <li><button v-on:click="logout" class="btn fas fa-sign-out-alt" :title="$t('LOGOUT')"/></li>
-          </ul>
+          </template>
+        </div>
       </div>
+      <BtnMenu/>
     </div>
   </div>
 </template>
@@ -63,6 +60,7 @@ import AccountService from '@/services/AccountService'
 import OperationForm from '@/components/forms/OperationForm.vue'
 import Utils from '@/utils/Utils'
 import PersonService from '@/services/PersonService'
+import BtnMenu from '@/components/ButtonMenu.vue'
 
 interface AccountPageData {
     operations: EditableOperation[];
@@ -77,7 +75,8 @@ interface EditableOperation extends Operation {
 export default defineComponent({
   name: 'AccountPage',
   components: {
-    OperationForm
+    OperationForm,
+    BtnMenu
   },
   beforeCreate: async function () {
     redirectToLoginPageIfNotLogged(this.$store)
