d5ceb1178e5c47c825ccb86a3f5d76bfed6f1353
erica <erica@tresorier.me>
2022-01-27 11:59:59 +0100

    update dao

--- src/main/kotlin/open/tresorier/model/Person.kt
+++ src/main/kotlin/open/tresorier/model/Person.kt
@@ -1,13 +1,16 @@
 package open.tresorier.model
+import open.tresorier.utils.Time
 
 class Person (
     var name: String,
     var hashedPassword: String,
     var email: String,
     var billingId: String? = null,
+    // true => up to date, false => not up to date, access ok, null => no billing status
     var billingStatus: Boolean? = null,
     var unlockingDate: Long = 0,
     var loginAttemptCount: Int = 0,
     id: String? = null,
-    deleted: Boolean? = null
+    deleted: Boolean? = null,
+    val creationDate: Long = Time.now(),
 ) : DbObject(id, deleted)
