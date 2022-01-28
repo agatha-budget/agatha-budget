4d5008b5f645cf11f8a851001bda4d57da988efd
erica <erica@tresorier.me>
2022-01-28 12:10:32 +0100

    properties

--- properties.ts
+++ properties.ts
@@ -1,7 +1,7 @@
 enum Properties {
     server = 'https://tresorier-back.herokuapp.com/',
     refreshUrl = 'https://tresorier-back.herokuapp.com/session/refresh',
-    commitHash = 'fe611ecf29b6b6861494771a8c71333cf1517866'
+    commitHash = '71b06b349da03297e7f689ffbc944e6d6a39843b'
 }
 
 export default Properties
