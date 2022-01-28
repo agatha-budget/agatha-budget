ba8bcffd04689cc0eef8fc739f0e60b5c18fa4b4
erica <erica@tresorier.me>
2022-01-28 19:08:04 +0100

    replace url

--- scripts/replace_commit_hash.sh
+++ scripts/replace_commit_hash.sh
@@ -1,2 +1,2 @@
 sed -i "s/commitHash = '.*'/commitHash = '$1'/g" properties.ts
-sed -i "s/tresorier-back\.herokuapp\.com/api.agatha-budget.fr/g" properties.ts
+sed -i "s/agatha-alternance\.herokuapp\.com/api.agatha-budget.fr/g" properties.ts
