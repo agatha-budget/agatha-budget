024ba2d32b40f4526255d9f295f92321a9ff936d
Raphael Agenet <raphael.agenet@gmail.com>
2022-02-15 15:16:12 +0100

    Profile page création

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,17 +1,57 @@
 <template>
   <div id="profile_page" :class="this.$store.state.css">
-    <h1>Profil utilisateur</h1>
-    <h3>Cette page est encore en construction</h3>
-    <p>Si vous avez été redirigé automatiquement ici, c'est que votre abonnement n'est pas à jour</p>
-    <p><a class="teamSelector" v-on:click="goToBillingPortal">{{$t('BILLING_MANAGEMENT')}}</a></p>
+    <div class="card-container">
+      <div class="upper-container">
+        <div class="image-container">
+           <img id="logoface" alt="Vue logoface" src="../assets/logo_round.png" />
+        </div>
+      </div>
+      <div class="lower-container">
+            <h3 class="ProfilName">Nom Prenom</h3>
+        <div>
+          <header>
+          <li>
+            <ul>
+              <a class="btnprofil fas fa-credit-card" v-on:click="goToBillingPortal">{{
+                $t("SUBCRIPTION")
+              }}</a>
+            </ul>
+            <ul>
+              <a class="btnprofil fas fa-book" v-on:click="goToBillingPortal">{{
+                $t("FREE_RESOURCE")
+              }}</a>
+            </ul>
+            <ul>
+              <a class="btnprofil fas fa-palette" v-on:click="apearance">{{
+                $t("APPEARANCE")
+              }}</a>
+            </ul>
+            <ul>
+              <a class="btnprofil fab fa-discord"  v-on:click="goToBillingPortal">{{
+                $t("COMUNITY")
+              }}</a>
+            </ul>
+            <ul>
+              <a class="btnprofil fas fa-power-off" v-on:click="login">{{
+                $t(id="LOGOUT")
+              }}</a>
+            </ul>
+          </li>
+          </header>
+          </div>
+    <div class="navprofile">
     <NavMenu :page="'profile'" />
+    </div>
+        </div>
+      </div>
   </div>
 </template>
- 
+
 <script lang="ts">
 import { defineComponent } from 'vue'
 import PersonService from '@/services/PersonService'
 import NavMenu from '@/components/NavigationMenu.vue'
+import router, { RouterPages } from '@/router'
 
 export default defineComponent({
   name: 'Profile',
@@ -25,8 +65,10 @@ export default defineComponent({
     },
     logout () {
       PersonService.deleteSession(this.$store)
+    },
+    goChooseAccount () {
+      router.push(RouterPages.redirectToAccountPage)
     }
   }
-
 })
 </script>
