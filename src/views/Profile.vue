06bd7016b82d3960c352db885943919828b74bae
Raphael Agenet <raphael.agenet@gmail.com>
2022-02-24 10:58:56 +0100

    Profil page update

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,74 +1,63 @@
 <template>
-  <div id="profile_page" :class="this.$store.state.css">
-    <div class="card-container">
-      <div class="upper-container"></div>
-      <div class="lower-container">
-        <div class="container-cadre">
-          <div class="image-container">
+  <div :class="this.$store.state.css">
+    <div class="profilpage">
+      <div class="col-12 offset-0 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-4 offset-lg-4 col-xxl-3 offset-xxl-5 ">
+        <div class="image-container">
            <img id="logoface" alt="Vue logoface" src="../assets/logo_round.png" />
         </div>
-            <h3 class="ProfilName">Nom Prenom</h3>
-        <div>
-          <header>
-          <li>
+         <h3 class="ProfilName">Nom Prenom</h3>
+        <li>
             <ul>
-              <a class="btnprofil fas fa-credit-card" v-on:click="goToBillingPortal">{{
+              <a class="buttonprofile" v-on:click="disabled"><div class="icone fas fa-credit-card"></div>{{
                 $t("SUBCRIPTION")
               }}</a>
             </ul>
             <ul>
-              <a class="btnprofil fas fa-book" v-on:click="goToBillingPortal">{{
+              <a class="buttonprofile" v-on:click="disabled"><div class="icone fas fa-book"></div>{{
                 $t("FREE_RESOURCE")
               }}</a>
             </ul>
             <ul>
-              <a class="btnprofil fas fa-palette" v-on:click="apearance">{{
+              <a class="buttonprofile" v-on:click="disabled"><div class="icone fas fa-palette"></div>{{
                 $t("APPEARANCE")
               }}</a>
             </ul>
             <ul>
-              <a class="btnprofil fab fa-discord"  v-on:click="goToBillingPortal">{{
+              <a class="buttonprofile"  v-on:click="disabled"><div class="icone fab fa-discord"></div>{{
                 $t("COMUNITY")
               }}</a>
             </ul>
             <ul>
-              <a class="btnprofil fas fa-power-off" v-on:click="login">{{
+              <a class="buttonprofile" v-on:click="logout"><div class="icone fas fa-power-off"></div>{{
                 $t(id="LOGOUT")
               }}</a>
             </ul>
           </li>
-          </header>
+          <div class="navmenu">
+          <NavMenu :page="'profile'" />
           </div>
-          </div>
-    <div class="navprofile">
-    <NavMenu :page="'profile'" />
-    </div>
-        </div>
       </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import PersonService from '@/services/PersonService'
 import NavMenu from '@/components/NavigationMenu.vue'
-import router, { RouterPages } from '@/router'
+import PersonService from '@/services/PersonService'
+import router, { RouterPages, redirectToLoginPageIfNotLogged } from '@/router'
 
 export default defineComponent({
-  name: 'Profile',
-  components: { NavMenu },
-  data () {
-    return {}
+  name: 'profilePage',
+  components: {
+    NavMenu
   },
   methods: {
-    async goToBillingPortal (): Promise<void> {
-      PersonService.redirectToBillingPortalUrl()
-    },
     logout () {
       PersonService.deleteSession(this.$store)
     },
-    goChooseAccount () {
-      router.push(RouterPages.redirectToAccountPage)
+    goToProfile () {
+      router.push(RouterPages.profile)
     }
   }
 })
