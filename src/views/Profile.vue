934cdce3501f40bcc893dffc87a772a5e41799c5
Raphael Agenet <raphael.agenet@gmail.com>
2022-02-28 15:36:21 +0100

    Update Profilpage with correction of problems in comments for the pullrequest

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,42 +1,42 @@
 <template>
   <div :class="this.$store.state.css">
-    <div class="profilpage">
-      <div class="col-12 offset-0 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-4 offset-lg-4 col-xxl-3 offset-xxl-5 ">
+    <div class="profilPage">
+      <div class="boxMenu col-12 offset-0 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-4 offset-lg-4 col-xxl-3 offset-xxl-5 ">
         <div class="image-container">
            <img id="logoface" alt="Vue logoface" src="../assets/logo_round.png" />
         </div>
-         <h3 class="ProfilName">Nom Prenom</h3>
+         <h3 class="profilName">{{$t("LASTNAME_FIRSTNAME")}}</h3>
         <li>
             <ul>
-              <a class="buttonprofile" v-on:click="disabled"><div class="icone fas fa-credit-card"></div>{{
+              <a class="buttonProfile" v-on:click="goToBillingPortal"><div class="icone fas fa-credit-card"></div>{{
                 $t("SUBCRIPTION")
               }}</a>
             </ul>
             <ul>
-              <a class="buttonprofile" v-on:click="disabled"><div class="icone fas fa-book"></div>{{
+              <a class="buttonProfile disabled"><div class="icone fas fa-book"></div>{{
                 $t("FREE_RESOURCE")
               }}</a>
             </ul>
             <ul>
-              <a class="buttonprofile" v-on:click="disabled"><div class="icone fas fa-palette"></div>{{
+              <a class="buttonProfile disabled"><div class="icone fas fa-palette"></div>{{
                 $t("APPEARANCE")
               }}</a>
             </ul>
             <ul>
-              <a class="buttonprofile"  v-on:click="disabled"><div class="icone fab fa-discord"></div>{{
-                $t("COMUNITY")
+              <a class="buttonProfile disabled"  ><div class="icone fab fa-discord"></div>{{
+                $t("COMMUNITY")
               }}</a>
             </ul>
             <ul>
-              <a class="buttonprofile" v-on:click="logout"><div class="icone fas fa-power-off"></div>{{
-                $t(id="LOGOUT")
+              <a class="buttonProfile" v-on:click="logout"><div class="icone fas fa-power-off"></div>{{
+                $t("LOGOUT")
               }}</a>
             </ul>
           </li>
-          <div class="navmenu">
-          <NavMenu :page="'profile'" />
-          </div>
       </div>
+    </div>
+    <div class="navMenu">
+    <NavMenu :page="'profile'" />
     </div>
   </div>
 </template>
@@ -45,7 +45,6 @@
 import { defineComponent } from 'vue'
 import NavMenu from '@/components/NavigationMenu.vue'
 import PersonService from '@/services/PersonService'
-import router, { RouterPages, redirectToLoginPageIfNotLogged } from '@/router'
 
 export default defineComponent({
   name: 'profilePage',
@@ -56,8 +55,8 @@ export default defineComponent({
     logout () {
       PersonService.deleteSession(this.$store)
     },
-    goToProfile () {
-      router.push(RouterPages.profile)
+    async goToBillingPortal (): Promise<void> {
+      PersonService.redirectToBillingPortalUrl()
     }
   }
 })
