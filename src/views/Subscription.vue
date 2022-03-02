b007a38f3096cfec5c4680c0d4290f4fda43e190
Claire <claire@agatha.fr>
2022-03-02 11:19:43 +0100

    add personal side and solidarity

--- src/views/Subscription.vue
+++ src/views/Subscription.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="this.$store.state.css">
-    <div id="subscriptionPage" class="col-12 offset-0 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-6 offset-lg-3 col-xxl-4 offset-xxl-4">
+    <div id="subscriptionPage" class="col-12 offset-0 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-6 offset-lg-3">
       <div v-if="profileTest == 'user'" class="user">
         <p class="col-12">{{ $t('TEXT_1_MONTH_FREE') }}</p>
         <div class="essential row">
@@ -25,13 +25,21 @@
             <button v-on:click="this.goToBillingPortal('ANNUAL_INTEGRAL')">{{ $t('PRICE_ANNUAL_INTEGRAL') }}</button>
           </div>
         </div>
-        <p class="col-12">{{ $t('TEXT_FOR_USER') }}</p>
+        <div class="solidarity">
+          <span class="icon icon-support col-12" />
+          <h1 class="title col-12">{{ $t('SOLIDARITY') }}</h1>
+          <p class="col-12" v-on:click="this.goToContactPage">{{ $t('TEXT_FOR_USER') }}{{ $t('PLEASE_CONTACT_US') }}</p>
+          <div class="btnSubscription disabled col-12">
+            <button v-on:click="this.goToContactPage">{{ $t('PRICE_MONTHLY_SOLIDARITY') }}</button>
+          </div>
+        </div>
       </div>
+
       <div v-if="profileTest == 'company'" class="company">
         <p class="col-12">{{ $t('TEXT_1_MONTH_FREE') }}</p>
-        <div class="subscription">
+        <div class="businessSide">
           <span class="icon icon-briefcase col-12" />
-          <h1 class="title col-12">{{ $t('COMPANY') }}</h1>
+          <h1 class="title col-12">{{ $t('BUSINESS_SIDE') }}</h1>
           <p class="col-12">{{ $t('TEXT_COMPANY') }}</p>
           <div class="btnSubscription col-6">
             <button v-on:click="this.goToBillingPortal('MONTHLY_COMPANY')">{{ $t('PRICE_MONTHLY_COMPANY') }}</button>
@@ -40,18 +48,28 @@
             <button v-on:click="this.goToBillingPortal('ANNUAL_COMPANY')">{{ $t('PRICE_ANNUAL_COMPANY') }}</button>
           </div>
         </div>
+        <div class="personalSide">
+          <span class="icon icon-home col-12" />
+          <h1 class="title col-12">{{ $t('PERSONAL_SIDE') }}</h1>
+          <p class="col-12" v-on:click="this.goToContactPage">{{ $t('TEXT_PERSONAL_SIDE') }}{{ $t('PLEASE_CONTACT_US') }}</p>
+          <div class="btnSubscription disabled col-6">
+            <button v-on:click="this.goToContactPage">{{ $t('PRICE_PERSONAL_SIDE_MONTHLY') }}</button>
+          </div>
+          <div class="btnSubscription disabled col-6">
+            <button v-on:click="this.goToContactPage">{{ $t('PRICE_PERSONAL_SIDE_ANNUAL') }}</button>
+          </div>
+        </div>
         <div class="coaching">
           <span class="icon icon-compass col-12" />
           <h1 class="title col-12">{{ $t('PERSONAL_SUPPORT') }}</h1>
-          <p class="col-12">{{ $t('TEXT_PERSONAL_SUPPORT') }}</p>
+          <p class="col-12" v-on:click="this.goToContactPage">{{ $t('TEXT_PERSONAL_SUPPORT') }}{{ $t('PLEASE_CONTACT_US_AND_RDV') }}</p>
           <div class="btnSubscription disabled col-6">
-            <button>{{ $t('PRICE_COACHING_1H') }}</button>
+            <button v-on:click="this.goToContactPage">{{ $t('PRICE_COACHING_1H') }}</button>
           </div>
           <div class="btnSubscription disabled col-6">
-            <button>{{ $t('PRICE_COACHING_5H') }}</button>
+            <button v-on:click="this.goToContactPage">{{ $t('PRICE_COACHING_5H') }}</button>
           </div>
         </div>
-        <p class="col-12">{{ $t('TEXT_FOR_COMPANY') }}</p>
       </div>
       <button v-on:click="changerProfileTest">changer de profil</button>
       <div class="placeholderBottom"/>
@@ -88,11 +106,11 @@ export default defineComponent({
     logout () {
       PersonService.deleteSession(this.$store)
     },
-
     async goToBillingPortal (selectedPackage: string): Promise<void> {
-      console.log(this.profile)
-      console.log(selectedPackage)
       PersonService.redirectToBillingPortalUrl(selectedPackage)
+    },
+    goToContactPage () {
+      window.location.href = 'https://agatha-budget.fr/contact/'
     },
 
     changerProfileTest () {
