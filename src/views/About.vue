c940614a9329e6cd045344a9ab233897c278e5ab
erica <erica@tresorier.me>
2022-04-01 12:01:00 +0200

    add hover and disabled for nav button

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,12 +2,17 @@
   <div>
     <h1 class="title">Default theme</h1>
     <btn class="navigationButton">Navigation button</btn>
+    <btn class="navigationButton disabled">Navigation button</btn>
   </div>
 
   <div class="dyslexia">
     <h1 class="title">dyslexia</h1>
-    <btn class="navigationButton">Navigation button</btn>
+    <div class="row">
+    <btn class="navigationButton col-6 offset-2">Navigation button</btn>
+    <btn class="navigationButton disabled col-6">Navigation button</btn>
+    </div>
   </div>
+
 </template>
 <style lang="less">
   @import "../style/atomic_design/atomic_design.less";
