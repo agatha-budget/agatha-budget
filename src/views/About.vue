124a67d1cf44d7615ec9946b3b953cc27f169d3b
erica <erica@tresorier.me>
2022-04-01 11:44:18 +0200

    add navigation button

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
-    <h1 class="title">This is an about page</h1>
+    <h1 class="title">Default theme</h1>
+    <btn class="navigationButton">Navigation button</btn>
   </div>
 
   <div class="dyslexia">
-    <h1 class="title">This is an about page</h1>
+    <h1 class="title">dyslexia</h1>
+    <btn class="navigationButton">Navigation button</btn>
   </div>
-
 </template>
 <style lang="less">
   @import "../style/atomic_design/atomic_design.less";
