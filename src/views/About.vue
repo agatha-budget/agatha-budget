f101ec884c4f17b1b98be851caee225d808b02db
erica <erica@tresorier.me>
2022-04-01 10:57:42 +0200

    apply design system to about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 class="title">This is an about page</h1>
   </div>
 </template>
+<style lang="less">
+  @import "../style/atomic_design/atomic_design.less";
+</style>
