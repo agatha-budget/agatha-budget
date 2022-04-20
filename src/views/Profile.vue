7ff1538a08b262bf05e73a7c71f459a5450f9814
Claire <claire@agatha.fr>
2022-04-20 18:05:09 +0200

    CSS applied on profile page

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,36 +1,33 @@
 <template>
   <div :class="this.$store.state.css">
-    <div class="profilPage">
-      <div class="boxMenu col-12 offset-0 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-4 offset-lg-4 col-xxl-3 offset-xxl-5 ">
+    <div class="profilePage">
+      <div class="boxMenu col-12 offset-0 col-md-6 offset-md-3 col-lg-4 offset-lg-4 col-xxl-3 offset-xxl-5 ">
         <div class="image-container">
            <img id="logoface" alt="Vue logoface" src="../assets/logo_round.png" />
         </div>
-        <li>
-            <ul>
-              <a class="buttonProfile row" v-on:click="goSubscriptionPage">
-                <div class="icone fas fa-credit-card col-4"></div>
-                <div class="label col-8">{{ $t("SUBSCRIPTION") }}</div>
-              </a>
-            </ul>
-            <ul>
-              <a class="buttonProfile disabled row">
-                <div class="icone fas fa-book col-4"></div>
-                <div class="label col-8">{{ $t("FREE_RESOURCES") }}</div>
-              </a>
-            </ul>
-            <ul>
-              <a class="buttonProfile disabled row">
-                <div class="icone fas fa-palette col-4"></div>
-                <div class="label col-8">{{ $t("APPEARANCE") }}</div>
-              </a>
-            </ul>
-            <ul>
-              <a class="buttonProfile row" href="https://forum.agatha-budget.fr">
-                <div class="icone fas fa-question col-4"></div>
-                <div class="label col-8">{{ $t("SUPPORT") }}</div>
-              </a>
-            </ul>
-          </li>
+        <div class="title">{{ $t('PREFERENCES') }}</div>
+        <div class="buttonContainer row">
+          <btn class="navigationButton col-10 offset-1 col-sm-6 offset-sm-3 col-md-10 offset-md-1 row" v-on:click="goSubscriptionPage">
+            <span class="illustration fas fa-credit-card col-4"/>
+            <div class="name col-8">{{ $t("SUBSCRIPTION") }}</div>
+          </btn>
+          <btn class="navigationButton disabled col-10 offset-1 col-sm-6 offset-sm-3 col-md-10 offset-md-1 row">
+            <span class="illustration fas fa-book-open col-4"/>
+            <div class="name col-8">{{ $t("FREE_RESOURCES") }}</div>
+          </btn>
+          <btn class="navigationButton disabled col-10 offset-1 col-sm-6 offset-sm-3 col-md-10 offset-md-1 row">
+            <span class="illustration fas fa-chart-area col-4"/>
+            <div class="name col-8">{{ $t("GRAPH") }}</div>
+          </btn>
+          <btn class="navigationButton disabled col-10 offset-1 col-sm-6 offset-sm-3 col-md-10 offset-md-1 row">
+            <span class="illustration fas fa-palette col-4"/>
+            <div class="name col-8">{{ $t("APPEARANCE") }}</div>
+          </btn>
+          <a class="navigationButton col-10 offset-1 col-sm-6 offset-sm-3 col-md-10 offset-md-1 row" href="https://forum.agatha-budget.fr">
+            <span class="illustration fas fa-question col-4"/>
+            <div class="name col-8">{{ $t("SUPPORT") }}</div>
+          </a>
+        </div>
       </div>
     </div>
     <div class="navMenu">
