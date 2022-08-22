22b72927b9059308a0a7c37da342f2c756b0ec73
erica <erica@tresorier.me>
2022-08-22 18:03:15 +0200

    add get string from timestamp

--- src/main/kotlin/open/tresorier/utils/Time.kt
+++ src/main/kotlin/open/tresorier/utils/Time.kt
@@ -2,6 +2,7 @@ package open.tresorier.utils
 
 import open.tresorier.model.Month
 import java.util.*
+import java.text.SimpleDateFormat
 
 object Time {
 
@@ -41,6 +42,12 @@ object Time {
         val date = Calendar.getInstance(TimeZone.getTimeZone("GMT"))
         date.set(givenMonth.year, givenMonth.month -1, 1, 0,0,0)
         return date.timeInMillis/1000
+    }
+
+    fun getDateStringFromTimestamp(timestamp: Long) : String {
+        val date = Date(timestamp)
+        val format = SimpleDateFormat("yyyy-MM-dd")
+        return format.format(date)
     }
 
 }
