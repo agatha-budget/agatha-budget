15298ba755cd0e202f83bf6f08d4b813bcb56162
erica <erica@tresorier.me>
2022-10-03 20:41:02 +0200

    update services

--- src/services/OperationService.ts
+++ src/services/OperationService.ts
@@ -1,12 +1,12 @@
-import { Account, Operation } from '@/model/model'
+import { Account, Operation, OperationWithDaughters } from '@/model/model'
 import { operationApi } from '@/services/api/apis'
 import StoreHandler from '@/store/StoreHandler'
 import { StoreState } from '@/store/index'
 import { Store } from 'vuex'
 
 export default class OperationService {
-  public static async getOperations (account: Account, categoryId: string | null): Promise<Operation[]> {
-    let data: Operation[] = []
+  public static async getOperations (account: Account, categoryId: string | null): Promise<OperationWithDaughters[]> {
+    let data: OperationWithDaughters[] = []
     if (account.id) {
       let response
       if (categoryId) {
@@ -39,30 +39,6 @@ export default class OperationService {
   public static async importOfxFile (store: Store<StoreState>, accountId: string, ofxFileContent: string): Promise<string> {
     const response = await operationApi.importOfxFile(accountId, ofxFileContent)
     StoreHandler.updateAccounts(store)
-    return response.data
-  }
-
-  public static async getMotherOperationsByAccount (account: Account, categoryId: string | null): Promise<Operation[]> {
-    let data: Operation[] = []
-    if (account.id) {
-      let response
-      if (categoryId) {
-        response = await operationApi.findMotherOperationsByAccount(account.id, categoryId)
-      } else {
-        response = await operationApi.findMotherOperationsByAccount(account.id, undefined)
-      }
-      data = response.data
-    }
-    return data
-  }
-
-  public static async getDaughtersFromMother (operation: Operation): Promise<Operation[]> {
-    const response = await operationApi.findDaughterOperationsByMother(operation.id)
-    return response.data
-  }
-
-  public static async getMotherFromDaughter (operation: Operation): Promise<Operation> {
-    const response = await operationApi.findMotherOperationsByDaughter(operation.id)
     return response.data
   }
 }
