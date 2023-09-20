349e0b67cb338baa1f401134cee8d5884b9eef30
Erica Delagnier <erica@agatha-budget.fr>
2023-09-20 15:42:41 +0200

    isolate unprotected route

---
+++ src/main/kotlin/open/tresorier/api/theme/unprotectedApi.kt
@@ -0,0 +1,39 @@
+package open.tresorier.api.theme
+
+import io.javalin.Javalin
+import open.tresorier.model.enum.ProfileEnum
+import open.tresorier.utils.Properties
+import open.tresorier.utils.PropertiesEnum.*
+import open.tresorier.services.PersonService
+import open.tresorier.exception.TresorierException
+import open.tresorier.api.*
+import open.tresorier.model.*
+
+fun addUnprotectedRoute(app : Javalin, properties: Properties, personService: PersonService) : Javalin {
+
+    app.get("/") { ctx ->
+        ctx.result("Hello Sunshine !")
+    }
+
+    app.get("/error") { ctx ->
+        val exception = TresorierException("this is your doing", Exception("why ?"))
+        ctx.result(exception.id)
+    }
+
+    app.get("/ping") { ctx ->
+        ctx.result(properties.get(ENVIRONMENT))
+    }
+
+    app.post("/signup") { ctx ->
+        val name = getQueryParam<String>(ctx, "name")
+        val password = getQueryParam<String>(ctx, "password")
+        val email = getQueryParam<String>(ctx, "email")
+        val profileString = getQueryParam<String>(ctx,"profile")
+        val profile: ProfileEnum = ProfileEnum.valueOf(profileString)
+        val person: Person = personService.createPerson(name, password, email, profile)
+       // SuperTokens.newSession(ctx, person.id).create()
+        ctx.json("{\"name\" : " + person.name + "}")
+    }
+    
+    return app
+}
