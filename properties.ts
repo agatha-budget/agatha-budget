b1f357a2f81910d4674410e8d57b3fa99426c72b
Erica Delagnier <erica@agatha-budget.fr>
2023-10-05 13:52:51 +0200

    set dependencies to last

--- properties.ts
+++ properties.ts
@@ -1,6 +1,6 @@
 enum Properties {
-    server = 'https://tresorier-back.herokuapp.com/',
-    refreshUrl = 'https://tresorier-back.herokuapp.com/session/refresh',
+    server = 'https://api.agatha-budget.fr/',
+    refreshUrl = 'https://api.agatha-budget.fr/session/refresh',
     commitHash = '55df010cdf38b139dab95e45a2df9a270a5369a7'
 }
 
--- shell.nix
+++ shell.nix
@@ -5,7 +5,7 @@ with pkgs;
 mkShell {
 # seems we should use jdk11_headless for server env
 buildInputs = [
-nodejs-16_x jdk11 git
+nodejs-18_x jdk11 git
 ];
 shellHook = ''
         export PATH="$PWD/node_modules/.bin/:$PATH"
