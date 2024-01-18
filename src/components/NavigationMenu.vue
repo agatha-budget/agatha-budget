5a59d78525d81f502a80f1bf5977aff4be1825c5
Erica Delagnier <erica.delagnier@multi.coop>
2024-01-18 14:29:54 +0100

    misc css and display

--- src/components/NavigationMenu.vue
+++ src/components/NavigationMenu.vue
@@ -5,8 +5,6 @@
         <li v-else><button v-on:click="goHomePage" class="navBarBtn btn home fas fa-home" :title="$t('HOME')"/></li>
         <li v-if="fromPage == 'accounts' "><button class="navBarBtn btn account fas fa-euro-sign disabled" :title="$t('ACCOUNTS')"/></li>
         <li v-else><button v-on:click="goChooseAccount" class="navBarBtn btn account fas fa-euro-sign" :title="$t('ACCOUNTS')"/></li>
-         <li v-if="fromPage == 'chart' "><button class="navBarBtn btn chart fas fa-chart-line disabled" :title="$t('CHART')"/></li>
-        <li v-else><button v-on:click="goChartPage" class="navBarBtn btn chart fas fa-chart-line" :title="$t('CHART')"/></li>
         <li v-if="fromPage == 'profile' "><button class="navBarBtn btn profile fas fa-cog disabled" :title="$t('PREFERENCES')"/></li>
         <li v-else><button v-on:click="goProfilePage" class="navBarBtn btn profile fas fa-cog" :title="$t('PREFERENCES')"/></li>
       </ul>
@@ -14,8 +12,8 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import router, { RouterPages } from '@/router'
+import router, { RouterPages } from '@/router';
+import { defineComponent } from 'vue';
 
 interface MenuData {
     fromPage: string;
