8bc2896032b7e898994127bb8a3e6a5988f0caec
edelagnier <erica.delagnier@multi.coop>
2024-06-20 15:36:25 +0200

    setup auto serve

--- front/properties.ts
+++ front/properties.ts
@@ -1,6 +1,6 @@
 enum Properties {
-    server = 'https://betapi.agatha-budget.fr',
-    commitHash = 'ab9c4be525935c3265ce2353832332307b7cbd53'
+    server = 'http://localhost:8000',
+    commitHash = 'a5a61c2846d723cf56737fcd49757dc397d09d91'
 }
 
 export default Properties
