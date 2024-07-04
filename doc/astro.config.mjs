24489f87bf691c2024a681980441564350f34c00
edelagnier <erica.delagnier@multi.coop>
2024-07-04 17:32:53 +0200

    update base

--- doc/astro.config.mjs
+++ doc/astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://agatha-budget.github.io',
-    base: '/',
+    base: '/agatha-budget',
     integrations: [
         starlight({
             title: 'Documentation',
