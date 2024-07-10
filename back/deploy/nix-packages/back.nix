6a66cee6f58acef493de93d55e8ee67702e20c76
edelagnier <erica.delagnier@multi.coop>
2024-07-10 17:24:46 +0200

    working

--- back/deploy/nix-packages/back.nix
+++ back/deploy/nix-packages/back.nix
@@ -26,19 +26,29 @@ stdenv.mkDerivation rec {
   name = "agatha-back";
   version = "2.4";
 
-  src = ../../build/libs/tresorier-backend-uber.jar;
-  dontUnpack = true;
+  src = [
+    ../../build/libs/tresorier-backend-uber.jar
+    ../../src/main/resources/db/pg
+  ];
+
+  unpackPhase = ''
+    runHook preUnpack
+
+    mkdir -pv share/agatha $out/bin
+    cp $src[0] share/agatha/${name}-${version}.jar
+    cp $src[1] share/agatha/migrations
+
+    runHook postUnpack
+  '';
 
   nativeBuildInputs = [ makeWrapper ];
 
   installPhase = ''
-  mkdir -pv $out/share/java $out/bin
-  cp ${src} $out/share/java/${name}-${version}.jar
-
-  makeWrapper ${jre}/bin/java $out/bin/agatha-back \
-    --add-flags "-Dlogback.configurationFile=logback.xml" \
-    --add-flags "-jar $out/share/java/${name}-${version}.jar" \
-    --set _JAVA_OPTIONS '-Dawt.useSystemAAFontSettings=on' \
-    --set _JAVA_AWT_WM_NONREPARENTING 1
+    cp -a share $out/share
+    makeWrapper ${jre}/bin/java $out/bin/agatha-back \
+      --add-flags "-Dlogback.configurationFile=logback.xml" \
+      --add-flags "-jar $out/share/agatha/${name}-${version}.jar" \
+      --set _JAVA_OPTIONS '-Dawt.useSystemAAFontSettings=on' \
+      --set _JAVA_AWT_WM_NONREPARENTING 1
   '';
 }
