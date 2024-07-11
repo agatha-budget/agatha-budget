d538f756bc14067785e2b253f0319077ebdfa46c
Erica Delagnier <erica.delagnier@multi.coop>
2024-07-11 12:32:10 +0200

    ♻️ - v 2.0.8 - update front dependencies
    * update dep
    
    * improve layout

--- front/properties.ts
+++ front/properties.ts
@@ -1,6 +1,6 @@
 enum Properties {
     server = 'http://localhost:8000',
-    commitHash = 'a5a61c2846d723cf56737fcd49757dc397d09d91'
+    commitHash = '44c88234b57bef1a72f8184f3d47e14f2a0d47d5'
 }
 
 export default Properties
