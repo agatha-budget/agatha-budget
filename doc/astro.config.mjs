3336958b239315c3d7fef76cb3de5427e7eda156
Erica Delagnier <erica.delagnier@multi.coop>
2024-09-12 16:57:08 +0200

    ♻️ - v 2.1.2 - update doc
    * change base url
    
    * Bump micromatch from 4.0.7 to 4.0.8 in /doc
    
    Bumps [micromatch](https://github.com/micromatch/micromatch) from 4.0.7 to 4.0.8.
    - [Release notes](https://github.com/micromatch/micromatch/releases)
    - [Changelog](https://github.com/micromatch/micromatch/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/micromatch/micromatch/compare/4.0.7...4.0.8)
    
    ---
    updated-dependencies:
    - dependency-name: micromatch
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump dset from 3.1.3 to 3.1.4 in /doc
    
    Bumps [dset](https://github.com/lukeed/dset) from 3.1.3 to 3.1.4.
    - [Release notes](https://github.com/lukeed/dset/releases)
    - [Commits](https://github.com/lukeed/dset/compare/v3.1.3...v3.1.4)
    
    ---
    updated-dependencies:
    - dependency-name: dset
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump pagefind from 1.1.0 to 1.1.1 in /doc
    
    Bumps [pagefind](https://github.com/CloudCannon/pagefind) from 1.1.0 to 1.1.1.
    - [Release notes](https://github.com/CloudCannon/pagefind/releases)
    - [Changelog](https://github.com/CloudCannon/pagefind/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/CloudCannon/pagefind/compare/v1.1.0...v1.1.1)
    
    ---
    updated-dependencies:
    - dependency-name: pagefind
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump @pagefind/default-ui from 1.1.0 to 1.1.1 in /doc
    
    Bumps @pagefind/default-ui from 1.1.0 to 1.1.1.
    
    ---
    updated-dependencies:
    - dependency-name: "@pagefind/default-ui"
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- doc/astro.config.mjs
+++ doc/astro.config.mjs
@@ -3,8 +3,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://agatha-budget.github.io',
-    base: '/agatha-budget',
+    site: 'https://doc.agatha-budget.fr',
+    base: '/',
     integrations: [
         starlight({
             title: 'Documentation',
