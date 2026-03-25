0e6fc7ee606eedac1a2bfa1631ea4a2bd32bae80
edelagnier <erica.delagnier@multi.coop>
2026-03-25 23:50:27 +0100

    update jjwt

--- back/build.gradle.kts
+++ back/build.gradle.kts
@@ -22,7 +22,7 @@ val javalin_version="6.4.0" // dec 2024
 val jackson_version="2.18.2" // nov 2024
 val stripe_version="28.3.1" // fevrier 2025 
 val json_version="20251224" // dec 2025
-val jjwt_version="0.12.6" // jun 2024
+val jjwt_version="0.13.0" // aug 2025
 
 
 buildscript {
